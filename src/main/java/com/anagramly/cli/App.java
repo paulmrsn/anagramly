22ef4a1bc25f38272ef0c38a65c8f1b4ab05ff34
Paul Muresan <17965058+paulmrsn@users.noreply.github.com>
2023-06-13 21:21:37 +0100

    Add better general exception handling and prettier cli interface.

--- src/main/java/com/anagramly/cli/App.java
+++ src/main/java/com/anagramly/cli/App.java
@@ -6,7 +6,11 @@ import com.anagramly.cli.processor.ParallelProcessor;
 import com.anagramly.cli.io.writer.ConsoleWriter;
 import com.anagramly.cli.io.writer.FileWriter;
 import com.anagramly.cli.io.writer.Writer;
+import com.github.lalyos.jfiglet.FigletFont;
 import picocli.CommandLine;
+
+import java.io.IOException;
+
 import static picocli.CommandLine.Command;
 import static picocli.CommandLine.Option;
 
@@ -17,19 +21,25 @@ public class App implements Runnable
     @Option(names = {"-f", "--file"}, required = true, description = "The file to process.")
     private String filePath;
 
-    @Option(names = {"-p", "--parallel"}, required = false, defaultValue = "false")
+    @Option(names = {"-p", "--parallel"}, defaultValue = "false")
     private boolean runInParallel;
 
-    @Option(names = {"-o", "--o"}, required = false)
+    @Option(names = {"-o", "--o"})
     private String outputPath;
 
     public static void main(String[] args) {
-        CommandLine.run(new App(), args);
+        try {
+            System.out.println(FigletFont.convertOneLine("Anagramly"));
+        } catch (IOException e) {
+            System.out.println("Could not display logo. " + e);
+        }
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
     @Override
     public void run() {
-        System.out.println("Processing file: " + filePath);
+        System.out.println("⏰ Processing file -> " + filePath);
         try {
             Writer writer;
             if(outputPath != null) {
@@ -46,9 +56,9 @@ public class App implements Runnable
                 processor = new LinearProcessor(writer);
                 processor.process(filePath);
             }
-            System.out.println("✔ Success: " + filePath);
+            System.out.println("✅ Success -> " + filePath);
         } catch (Exception e) {
-            System.out.println("Something went wrong for: " + filePath + ", err: " + e);
+            System.out.println("❌ Something went wrong for: " + filePath + ", err: " + e);
         }
     }
 }
